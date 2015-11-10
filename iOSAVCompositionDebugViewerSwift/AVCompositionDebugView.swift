4113848bb3d314630c96634c249438ff68354d99
Justin Winter <justinlevi@gmail.com>
2015-11-10 09:05:11 -0500

    Stubbing out AVCompositionDebugViewController
    - Also threw in the build script to convert :TODO's & FIXME's

---
+++ iOSAVCompositionDebugViewerSwift/AVCompositionDebugView.swift
@@ -0,0 +1,21 @@
+//
+//  AVCompositionDebugView.swift
+//  iOSAVCompositionDebugViewerSwift
+//
+//  Created by Justin Winter on 11/10/15.
+//  Copyright © 2015 soundslides. All rights reserved.
+//
+
+import UIKit
+
+class AVCompositionDebugView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- iOSAVCompositionDebugViewerSwift/AVCompositionDebugViewController.swift
+++ iOSAVCompositionDebugViewerSwift/AVCompositionDebugViewController.swift
@@ -7,19 +7,92 @@
 //
 
 import UIKit
+import AVFoundation
+
+/*
+    Player view backed by an AVPlayerLayer
+*/
+class APLPlayerView: UIView {
+  var player = AVPlayer()
+  
+  override var layer: AVPlayerLayer {
+    get{ return self.layer }
+    set{ self.layer = newValue }
+  }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    layer.player = player
+  }
+}
+
 
 class AVCompositionDebugViewController: UIViewController {
 
+  // ============================================
+  // MARK: -  Properties
+  
+  var playing = false
+  var scrubInFlight = false
+  var seekToZeroBeforePlayer = false
+  var lastScrubSliderValue = 0
+  var playRateToRestor = 0
+  
+  //  let timeObserver
+  var transitionDuration = 0
+  var transitionsEnabled = true
+  
+  let player: AVPlayer? = nil
+  let playerItem: AVPlayerItem? = nil
+  
+  
+  // ============================================
+  // MARK: -  Outlets
+  
+  @IBOutlet weak var playerView: APLPlayerView!
+  @IBOutlet weak var compositionDebugView: AVCompositionDebugView!
+  
+  @IBOutlet weak var scrubber: UISlider!
+  @IBOutlet weak var playPauseButton: UIButton!
+  @IBOutlet weak var currentTimeLabel: UILabel!
+  
+  
+  // ============================================
+  // MARK: -  Actions
+  
+  @IBAction func togglePlayPause(sender: UIButton){}
+  
+  @IBAction func beginScrubbing(sender: UISlider){}
+  @IBAction func scrub(sender: UISlider){}
+  @IBAction func endScrubbing(sender: UISlider){}
+  
+  
+  // ============================================
+  // MARK: -  ViewController LifeCycle
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
 
+  // ============================================
+  // MARK: -  Imperitives
+  
+  func updatePlayePauseButton() {}
+  
+  func updateScubber() {}
+  
+  func updateTimeLabel() {}
+  
+  func playerItemDuration() -> CMTime { return kCMTimeZero }
+  
+  func synchronizePlayerWithEditor() {}
 
 }
 
